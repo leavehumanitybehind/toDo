e2000e30fa6a1f21535626af5cb656acd46441af
ubitinonevami <bernastya@inbox.ru>
2021-04-18 16:29:23 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:ubitinonevami/toDo.git master:gh-pages
 cd -
